f7ece9d1498f7e2a37ef1cc96c48a8dda97d1253
pentekmate <erno801@gmail.com>
2023-02-28 11:40:37 +0100

    s

---
+++ adminscript.js
@@ -0,0 +1,39 @@
+let tovabb = false
+function Bejelentkezes() {
+
+    let felh = document.getElementById("felh").value
+    let jelsz = document.getElementById("jelsz").value
+    console.log(jelsz)
+    if (felh != "webAdmin") {
+        alert("Nem megfelelő felhasználónév.")
+    }
+    else {
+
+        var adatok = {
+            bevitel1: felh,
+            bevitel2: jelsz
+        }
+
+
+        fetch('http://localhost:3000/login', {
+            method: "POST",
+            body: JSON.stringify(adatok),
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        }
+        )
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log("asdssss", responseJson)
+                if (responseJson == false) {
+                    alert("Nem megfelelő adatok.")
+                }
+                else {
+                    window.location.href = "index.html"
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+
+    }
+}
