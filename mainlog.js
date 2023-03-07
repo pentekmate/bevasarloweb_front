a490d373a83a47d0fd5cea28b13e474970db6cec
pentekmate <erno801@gmail.com>
2023-03-07 11:54:39 +0100

    asd

--- mainlog.js
+++ mainlog.js
@@ -1,21 +1,19 @@
-let id="";
-let felhasznalo="";
-function logincheck()
-{   
+let id = "";
+let felhasznalo = "";
+function logincheck() {
     felhasznalo = sessionStorage.getItem("felhasznalo");
-    document.getElementById("belepettfh").innerHTML=felhasznalo
-    document.getElementById("belepettfh").style.color="rgb(1,194,154)"; 
+    document.getElementById("belepettfh").innerHTML = felhasznalo
+    document.getElementById("belepettfh").style.color = "rgb(1,194,154)";
     console.log(felhasznalo)
 }
-function kilepes()
-{
+function kilepes() {
     sessionStorage.clear()
     window.location.href = "main.html"
 }
 
-function getProfilId(){
-    let bemenet={
-        "bevitel1":felhasznalo
+function getProfilId() {
+    let bemenet = {
+        "bevitel1": felhasznalo
     }
     fetch('http://localhost:3000/getid', {
         method: "POST",
@@ -23,17 +21,17 @@ function getProfilId(){
         headers: { "Content-type": "application/json; charset=UTF-8" }
     }
     )
-    .then((response) => response.json())
-    .then((responseJson) => {
-    getProfilKep(responseJson)
-    })
-    .catch((error) => {
-        console.error(error);
-    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            getProfilKep(responseJson)
+        })
+        .catch((error) => {
+            console.error(error);
+        })
 }
-function getProfilKep(id){
-    let bemenet={
-        "bevitel1":id
+function getProfilKep(id) {
+    let bemenet = {
+        "bevitel1": id
     }
     fetch('http://localhost:3000/getprofilkep', {
         method: "POST",
@@ -41,45 +39,44 @@ function getProfilKep(id){
         headers: { "Content-type": "application/json; charset=UTF-8" }
     }
     )
-    .then((response) => response.json())
-    .then((responseJson) => {
-     megjelenit(responseJson)
-    })
-    .catch((error) => {
-        console.error(error);
-    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            megjelenit(responseJson)
+        })
+        .catch((error) => {
+            console.error(error);
+        })
 }
-function megjelenit(kep)
-{
-    let profkep='<img  src="./Kepek/'+kep+'.png" style="width:70px;height:70px">'
-    document.getElementById("profilkep").innerHTML=profkep
-   
+function megjelenit(kep) {
+    let profkep = '<img  src="./Kepek/' + kep + '.png" style="width:70px;height:70px">'
+    document.getElementById("profilkep").innerHTML = profkep
+
 }
 fetch("http://localhost:3000/getkomment")
     .then(x => x.json())
     .then(y => kommentek(y))
 
-function kommentek(tomb){
-    let sz="";
+function kommentek(tomb) {
+    let sz = "";
     console.log(tomb)
-    tomb.map((item)=>{
-    sz+=' <div class="row justify-content-center" style="padding-top: 5rem;">'
-    sz+='<div class="col-2 bg-dark position-relative">'
-    sz+=' <img src="Kepek/'+item.felhasznalo_kep_id+'.png"  style="width:80px;height:80px;border-radius: 50px;" alt="">'
-    sz+='<span  class="position-absolute top-0 start-1" style="color:rgb(1,194,154)">'+item.felhasznalo_nev+'</span>'
-    sz+='</div>'
-    sz+='<div class="col-4 bg-secondary min-vh-20 position-relative" style="border-radius: 5px;">'
-    sz+='<p  style="font-weight:600;font-size:16px">'+item.wm_szoveg+' </p>'
-    sz+= '<span  class="position-absolute bottom-0 end-0"  style="color:rgb(1,194,154)">'+item.wm_datum+'</span>'
-    sz+='</div>'
-    sz+='</div>'
-   
+    tomb.map((item) => {
+        sz += ' <div class="row justify-content-center" style="padding-top: 5rem;">'
+        sz += '<div class="col-2 bg-dark  position-relative" >'
+        sz += '<div style="background-color:red;width:80px;height:80px;border-radius:50px" class="bg-secondary"> <img src="Kepek/' + item.felhasznalo_kep_id + '.png"  style="width:80px;height:80px;border-radius: 50px;" alt=""> <span  class="position-absolute bottom-80 start-50" style="color:rgb(1,194,154);font-weight:600;">' + item.felhasznalo_nev + '</span> </div>'
+        sz += '</div>'
+        sz += '<div class="col-4 bg-secondary min-vh-20 position-relative" style="border-radius: 5px;">'
+        sz += '<p style="font-weight:400;font-size:16px">' + item.wm_szoveg + ' </p>'
+        sz += '<span  class="position-absolute bottom-0 end-0"  style="color:rgb(1,194,154)">' + item.wm_datum + '</span>'
+        sz += '</div>'
+        sz += '<div class="col-2 bg-danger "><div class="d-flex flex-row bd-highlight mb-3"> <div class="p-2 bd-highlight" ><i class="bi bi-hand-thumbs-up-fill" style="background-color:red"></i></div ><div class="p-2 bd-highlight">Flex item 2</div><div class="p-2 bd-highlight">Flex item 3</div></div ></div>'
+        sz += '</div>'
 
-    
-    
-    
-})
-    document.getElementById("megjelenit").innerHTML=sz
+
+
+
+
+    })
+    document.getElementById("megjelenit").innerHTML = sz
 }
 
 logincheck()
--- script.js
+++ script.js
@@ -1,7 +1,117 @@
 
-function bejelentkezes()
-{
-    
+function bejelentkezes() {
+
     window.location.href = 'login.html'
+}
+fetch("http://localhost:3000/getkomment")
+    .then(x => x.json())
+    .then(y => kommentek(y))
+
+function kommentek(tomb) {
+    let sz = "";
+    console.log(tomb)
+    tomb.map((item) => {
+        sz += ' <div class="row justify-content-center" style="padding-top: 5rem;">'
+        sz += '<div class="col-2 bg-dark  position-relative" >'
+        sz += '<div style="background-color:red;width:80px;height:80px;border-radius:50px" class="bg-secondary"> <img src="Kepek/' + item.felhasznalo_kep_id + '.png"  style="width:80px;height:80px;border-radius: 50px;" alt=""> <span  class="position-absolute bottom-80 start-50" style="color:rgb(1,194,154);font-weight:600;">' + item.felhasznalo_nev + '</span> </div>'
+        sz += ''
+        sz += '</div>'
+        sz += '<div class="col-4 bg-secondary min-vh-20 position-relative" style="border-radius: 5px;">'
+        sz += '<p  style="font-weight:400;font-size:16px">' + item.wm_szoveg + ' </p>'
+        sz += '<span  class="position-absolute bottom-0 end-0"  style="color:rgb(1,194,154)">' + item.wm_datum + '</span>'
+        sz += '</div>'
+        sz += '<div class="col-2 bg-dark "><div class="d-flex flex-row bd-highlight mb-3"> <div class="p-2 bd-highlight" ><i style="color: green;"  class="bi bi-hand-thumbs-up-fill"></i></div ><div class="p-2 bd-highlight"><i style="color:red" class="bi bi-hand-thumbs-down-fill"></i></div><div class="p-2 bd-highlight"><i style="color:yellow" class="bi bi-exclamation-square-fill"></i></div></div ></div>'
+        sz += '</div>'
+
+
+
+
+
+    })
+    document.getElementById("megjelenit").innerHTML = sz
+}
+
+const square = document.querySelector('.baloldalkep');
+square.classList.remove('balkepanimacio');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            square.classList.add('balkepanimacio');
+            return;
+        }
+
+        square.classList.remove('balkepanimacio');
+    });
+});
+observer.observe(document.querySelector('.elso'));
+//-----------------------------MÁSODIK----------------------------------
+const square1 = document.querySelector('.jobboldalkep');
+square.classList.remove('jobbkepanimacio');
+
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            square1.classList.add('jobbkepanimacio');
+            return;
+        }
+
+        square1.classList.remove('jobbkepanimacio');
+    });
+});
+observer1.observe(document.querySelector('.masodik'));
+//---------------------------HARMADIK-------------------------------------
+const square2 = document.querySelector('.baloldalkep1');
+square.classList.remove('balkepanimacio');
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            square2.classList.add('balkepanimacio');
+            return;
+        }
+
+        square2.classList.remove('balkepanimacio');
+    });
+});
+observer2.observe(document.querySelector('.harmadik'));
+//-----------------------------NEGYEDIK-----------------------------------
+const square3 = document.querySelector('.jobboldalkep1');
+square.classList.remove('jobbkepanimacio');
+
+const observer3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            square3.classList.add('jobbkepanimacio');
+            return;
+        }
+
+        square3.classList.remove('jobbkepanimacio');
+    });
+});
+observer3.observe(document.querySelector('.negyedik'));
+//----------------------------ÖTÖDIK-------------------------------------
+const square4 = document.querySelector('.baloldalkep2');
+square.classList.remove('balkepanimacio');
+
+const observer4 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            square4.classList.add('balkepanimacio');
+            return;
+        }
+
+        square4.classList.remove('balkepanimacio');
+    });
+});
+observer4.observe(document.querySelector('.otodik'));
+//--------------------------------PROGRESSBAR---------------------------------------
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
 }
 
